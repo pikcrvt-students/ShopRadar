74f93ac11f3b60e252e9bf1ef884c9253f50aebe
tjomss <A242531AP@rvt.lm>
2026-04-09 12:05:32 +0300

    Day 7

--- mongodb/ShopCart/product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("../db");
-
-const productSchema = new mongoose.Schema({
-    nosaukums: String,
-    cena: Number,
-    attels: String
-});
-
-module.exports = mongoose.model("Product", productSchema);
